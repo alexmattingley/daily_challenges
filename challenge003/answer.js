e155f356c0994df0d2c593c3c61343a5ecb02b73
Alex Mattingley <alexmattingley@gmail.com>
2015-08-10 07:50:42 -0700

    ok got the basic concept/idea

--- challenge003/answer.js
+++ challenge003/answer.js
@@ -2,6 +2,17 @@
 
 var array1 = [ [1,2,3], [3,4,5], [6,7,8] ]
 var array2 = [ [1,2,3], [1,2,3], [1,2,3] ]
+var output;
 
-console.log(array1);
-console.log(array2);
+
+function matrix_add() {
+
+	for(var i = 0; i <= 3; i++) {
+		console.log(array1[i]);
+		console.log(array2[i]);
+	}
+
+	output = array1[0][2] + array2[0][2];
+	console.log(output);
+}
+matrix_add();
