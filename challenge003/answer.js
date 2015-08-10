55bd716978c58ef72439f45debab372c8171ebcc
Alex Mattingley <alexmattingley@gmail.com>
2015-08-10 08:04:38 -0700

    got it! Maybe this nested for loop thing is getting easier for me?

--- challenge003/answer.js
+++ challenge003/answer.js
@@ -2,17 +2,20 @@
 
 var array1 = [ [1,2,3], [3,4,5], [6,7,8] ]
 var array2 = [ [1,2,3], [1,2,3], [1,2,3] ]
-var output;
+var output = [[], [], []];
 
 
 function matrix_add() {
 
-	for(var i = 0; i <= 3; i++) {
-		console.log(array1[i]);
-		console.log(array2[i]);
+	for(var i = 0; i <= 2; i++) {
+		// console.log(array1[i]);
+		// console.log(array2[i]);
+		for(var x = 0; x <= 2; x++) {
+			output[i][x] = array1[i][x] + array2[i][x];
+		}
 	}
 
-	output = array1[0][2] + array2[0][2];
+	//output = array1[0][2] + array2[0][2];
 	console.log(output);
 }
 matrix_add();
