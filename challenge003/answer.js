f4f5259f09df7a198a35d8a3fe4acd42bd033ca0
Alex Mattingley <alexmattingley@gmail.com>
2015-08-10 08:10:12 -0700

    there are a couple of limitations to this way of doing things. Number one we need even length arrays, if you have two arrays and one is longer or shorter than the other, we are going to get error messages. We are also making the assumption that we know the length of the array which can get messy quick. The output array REQUIRES empty brackets for each place holder and will not work without said place holders

--- challenge003/answer.js
+++ challenge003/answer.js
@@ -5,17 +5,17 @@ var array2 = [ [1,2,3], [1,2,3], [1,2,3] ]
 var output = [[], [], []];
 
 
-function matrix_add() {
+function matrix_add(array_1, array_2) {
 
 	for(var i = 0; i <= 2; i++) {
 		// console.log(array1[i]);
 		// console.log(array2[i]);
 		for(var x = 0; x <= 2; x++) {
-			output[i][x] = array1[i][x] + array2[i][x];
+			output[i][x] = array_1[i][x] + array_2[i][x];
 		}
 	}
 
 	//output = array1[0][2] + array2[0][2];
 	console.log(output);
 }
-matrix_add();
+matrix_add(array1, array2);
