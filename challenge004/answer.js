3d900b7e9fa5e8919398b84d190c0b5a5ea7e968
Alex Mattingley <alexmattingley@gmail.com>
2015-08-24 07:42:20 -0700

    no idea why this is not working, gonna go eat and approach again in a minute

--- challenge004/answer.js
+++ challenge004/answer.js
@@ -5,19 +5,16 @@ function int_test(num1){
     num1 % 1 === 0;
 }
 
-//int_test(10.5);
-
 var equals; //need a better var name
 
 function factor_test(num2){
     for(var i = 1; i <= num2; i++){
         equals = num2/i;
-        console.log(equals);
         if(int_test(equals)){
             output.push(equals);
         }
     }
 }
 
-factor_test(10);
+factor_test(12);
 console.log(output);
