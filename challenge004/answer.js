f080c3c4f60faff6e28d395fb959d3152936110f
Alex Mattingley <alexmattingley@gmail.com>
2015-08-24 07:33:25 -0700

    this is the general idea, but it needs work to function properly

--- challenge004/answer.js
+++ challenge004/answer.js
@@ -1,5 +1,23 @@
-function factor_test(num1){
-    console.log(num1 % 1 === 0);
+
+var output = [];
+
+function int_test(num1){
+    num1 % 1 === 0;
 }
 
-factor_test(10.5);
+//int_test(10.5);
+
+
+
+function factor_test(num1){
+    for(var i = 1; i <= num1; i++){
+        var equals; //need a better var name
+        equals = num1/i;
+        if(int_test(equals)){
+            output.push(equals);
+        }
+    }
+}
+
+factor_test(10);
+console.log(output);
